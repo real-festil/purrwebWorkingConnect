4b36a04cc69c6f6b808dce06bce3e530dddd1e77
Roman Gladyshev <romagladyshev@gmail.com>
2020-08-21 16:43:09 +0600

    Add all tab

--- src/App.js
+++ src/App.js
@@ -1,14 +1,70 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import managersJson from "./tableManager.json";
-import testersJson from "./tableTesters.json";
-import contentJson from "./tableContent.json";
-import designersJson from "./tableDesigners.json";
-import developersJson from "./tableDevelopers.json";
+
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import axios from "axios";
 
 function App() {
-  const tabs = ["managers", "testers", "content", "designers", "developers"];
+  const tabs = [
+    "all",
+    "managers",
+    "testers",
+    "content",
+    "designers",
+    "developers",
+  ];
   const [activeTab, setActiveTab] = useState("managers");
+  const [managersJson, setManagersJson] = useState();
+  const [testersJson, setTestersJson] = useState();
+  const [contentJson, setContentJson] = useState();
+  const [designersJson, setDesignersJson] = useState();
+  const [developersJson, setDevelopersJson] = useState();
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://spreadsheets.google.com/feeds/cells/1Z4AbwcBShKFdvWZmgB5ox59GW1MklFAFATxuY4Kv6sU/2/public/full?alt=json"
+      )
+      .then((res) => {
+        setManagersJson(res.data);
+      });
+    axios
+      .get(
+        "https://spreadsheets.google.com/feeds/cells/1Z4AbwcBShKFdvWZmgB5ox59GW1MklFAFATxuY4Kv6sU/3/public/full?alt=json"
+      )
+      .then((res) => {
+        setTestersJson(res.data);
+      });
+    axios
+      .get(
+        "https://spreadsheets.google.com/feeds/cells/1Z4AbwcBShKFdvWZmgB5ox59GW1MklFAFATxuY4Kv6sU/4/public/full?alt=json"
+      )
+      .then((res) => {
+        setDesignersJson(res.data);
+      });
+    axios
+      .get(
+        "https://spreadsheets.google.com/feeds/cells/1Z4AbwcBShKFdvWZmgB5ox59GW1MklFAFATxuY4Kv6sU/5/public/full?alt=json"
+      )
+      .then((res) => {
+        setContentJson(res.data);
+      });
+    axios
+      .get(
+        "https://spreadsheets.google.com/feeds/cells/1Z4AbwcBShKFdvWZmgB5ox59GW1MklFAFATxuY4Kv6sU/6/public/full?alt=json"
+      )
+      .then((res) => {
+        setDevelopersJson(res.data);
+      });
+  }, []);
+
+  const isReady =
+    managersJson &&
+    testersJson &&
+    designersJson &&
+    contentJson &&
+    developersJson;
 
   let managers;
   switch (activeTab) {
@@ -31,59 +87,115 @@ function App() {
       break;
   }
 
-  const managersList = managers.feed.entry
-    .filter((manager) => manager.gs$cell.row === "1")
-    .map((manager) => manager.gs$cell.$t);
+  const getWorkersToManagers = (managers) => {
+    const managersList = managers.feed.entry
+      .filter((manager) => manager.gs$cell.row === "1")
+      .map((manager) => manager.gs$cell.$t);
 
-  const workersList = managers.feed.entry
-    .filter((manager) => manager.gs$cell.row !== "1")
-    .map((manager) => manager.gs$cell.$t)
-    .filter((v, i, a) => a.indexOf(v) === i);
+    const workersList = managers.feed.entry
+      .filter((manager) => manager.gs$cell.row !== "1")
+      .map((manager) => manager.gs$cell.$t)
+      .filter((v, i, a) => a.indexOf(v) === i);
 
-  const managersLength = managers.feed.entry.filter(
-    (manager) => manager.gs$cell.row === "1"
-  ).length;
+    const managersLength = managers.feed.entry.filter(
+      (manager) => manager.gs$cell.row === "1"
+    ).length;
 
-  const managersToWorkers = [];
-  for (let i = 1; i < managersLength + 1; i++) {
-    managersToWorkers.push(
-      managers.feed.entry
-        .filter(
-          (manager) =>
-            manager.gs$cell.col === i.toString() && manager.gs$cell.row !== "1"
-        )
-        .map((manager) => manager.gs$cell.$t)
-    );
+    const managersToWorkers = [];
+    for (let i = 1; i < managersLength + 1; i++) {
+      managersToWorkers.push(
+        managers.feed.entry
+          .filter(
+            (manager) =>
+              manager.gs$cell.col === i.toString() &&
+              manager.gs$cell.row !== "1"
+          )
+          .map((manager) => manager.gs$cell.$t)
+      );
+    }
+
+    const workersToManagers = workersList.map((worker) => {
+      const a = managersToWorkers
+        .map((mng) => mng.find((x) => x === worker))
+        .map((q, i) => {
+          if (typeof q !== "undefined") return managersList[i];
+          else return undefined;
+        })
+        .filter((x) => x !== undefined);
+      return { worker, managers: a };
+    });
+    return workersToManagers;
+  };
+
+  let uniqueWorkers = [];
+
+  if (isReady) {
+    let allWorkers = [
+      ...getWorkersToManagers(managersJson),
+      ...getWorkersToManagers(testersJson),
+      ...getWorkersToManagers(developersJson),
+      ...getWorkersToManagers(contentJson),
+      ...getWorkersToManagers(designersJson),
+    ].map((worker) => ({
+      ...worker,
+      worker: worker.worker.trim(),
+    }));
+
+    const uniqueWorkersByName = allWorkers.reduce((acc, currentWorker) => {
+      const key = currentWorker.worker.trim();
+      const worker = acc[key];
+      if (worker) {
+        const newWorker = {
+          ...worker,
+          managers: [...worker.managers, ...currentWorker.managers],
+        };
+        return { ...acc, [key]: newWorker };
+      }
+      return { ...acc, [key]: currentWorker };
+    }, {});
+    uniqueWorkers = Object.values(uniqueWorkersByName);
   }
-
-  const workersToManagers = workersList.map((worker) => {
-    const a = managersToWorkers
-      .map((mng) => mng.find((x) => x === worker))
-      .map((q, i) => {
-        if (typeof q !== "undefined") return managersList[i];
-        else return undefined;
-      })
-      .filter((x) => x !== undefined);
-    return { worker, managers: a };
-  });
 
   return (
     <div className="App">
       <header>
         <div>
           {tabs.map((tab) => (
-            <button onClick={() => setActiveTab(tab)}>{tab}</button>
+            <button key={tab} onClick={() => setActiveTab(tab)}>
+              {tab}
+            </button>
           ))}
         </div>
       </header>
-      {workersToManagers.map((wtm) => (
-        <>
-          <h3>{wtm.worker}</h3>
-          {wtm.managers.map((m) => (
-            <p>{m}</p>
-          ))}
-        </>
-      ))}
+      {isReady ? (
+        activeTab === "all" ? (
+          uniqueWorkers.map((wtm) => (
+            <React.Fragment key={wtm.worker}>
+              <h3>{wtm.worker}</h3>
+              {wtm.managers.map((m) => (
+                <p key={m}>{m}</p>
+              ))}
+            </React.Fragment>
+          ))
+        ) : (
+          getWorkersToManagers(managers).map((wtm) => (
+            <React.Fragment key={wtm.worker}>
+              <h3>{wtm.worker}</h3>
+              {wtm.managers.map((m) => (
+                <p key={m}>{m}</p>
+              ))}
+            </React.Fragment>
+          ))
+        )
+      ) : (
+        <Loader
+          type="Puff"
+          color="#00BFFF"
+          height={100}
+          width={100}
+          timeout={3000} //3 secs
+        />
+      )}
     </div>
   );
 }
